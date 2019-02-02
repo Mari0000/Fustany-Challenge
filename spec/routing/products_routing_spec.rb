24a9e2cf60da0f656c0834a8dfa9e2c75ae302da
mariosobhy <mariosobhy0123@outlook.com>
2019-02-02 02:33:42 +0200

    fix rspec routing

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -3,35 +3,35 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/products').to route_to('products#index')
+      expect(get: 'categories/1/products').to route_to('products#index', category_id: '1')
     end
 
     it 'routes to #new' do
-      expect(get: '/products/new').to route_to('products#new')
+      expect(get: 'categories/1/products/new').to route_to('products#new', category_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/products/1').to route_to('products#show', id: '1')
+      expect(get: 'categories/1/products/1').to route_to('products#show', category_id: '1', id: '1')
     end
 
     it 'routes to #edit' do
-      expect(get: '/products/1/edit').to route_to('products#edit', id: '1')
+      expect(get: 'categories/1/products/1/edit').to route_to('products#edit', category_id: '1', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/products').to route_to('products#create')
+      expect(post: 'categories/1/products').to route_to('products#create', category_id: '1',)
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/products/1').to route_to('products#update', id: '1')
+      expect(put: 'categories/1/products/1').to route_to('products#update', category_id: '1', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/products/1').to route_to('products#update', id: '1')
+      expect(patch: 'categories/1/products/1').to route_to('products#update', category_id: '1', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/products/1').to route_to('products#destroy', id: '1')
+      expect(delete: 'categories/1/products/1').to route_to('products#destroy', category_id: '1', id: '1')
     end
   end
 end
